06591196aae161401f6cdb4357e8cd974450d1bc
Nikita Tarasov <connect@mygento.ru>
2016-05-25 15:42:02 +0300

    pace

--- app/code/community/Mygento/JQueryLib/Helper/Data.php
+++ app/code/community/Mygento/JQueryLib/Helper/Data.php
@@ -5,9 +5,9 @@
  *
  * @category Mygento
  * @package Mygento_JQueryLib
- * @copyright Copyright © 2015 NKS LLC. (http://www.mygento.ru)
+ * @copyright Copyright © 2016 NKS LLC. (http://www.mygento.ru)
  */
 class Mygento_JQueryLib_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+
 }
